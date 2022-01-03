cad1f6f48dd4c9849dfeaeac07b22a97616b4627
Pieter Meiresone <pieter.meiresone@gmail.com>
2022-01-03 15:13:21 +0100

    update hpc job file

--- Python_batch_neurodrone.sh
+++ Python_batch_neurodrone.sh
@@ -7,4 +7,4 @@ module load PyTorch/1.7.1-fosscuda-2020b
 module load OpenCV/4.5.1-fosscuda-2020b-contrib
 module load tensorboardX/2.2-fosscuda-2020b-PyTorch-1.7.1
 cd ~/hero_radar_odometry
-python train.py --pretrain models/under_the_radar_res2592.pt --config models/under_the_radar_neurodrone_res2592.json
+python train.py --pretrain models/under_the_radar_res2592.pt --config models/under_the_radar_neurodrone_res2592_hpc.json
